3a493031d613b1312afd95424da4b2cd86f6d0aa
Alejandro Amoroso <letzalet@gmail.com>
2024-11-05 16:31:58 -0300

    React router loader example, careers

--- new-router-nov2024/src/App.js
+++ new-router-nov2024/src/App.js
@@ -10,10 +10,15 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/help/Contact";
 import Faq from "./pages/help/Faq";
+import NotFound from "./pages/NotFound";
+
+//page and loader
+import Careers, { careersLoader } from "./pages/careers/Careers";
 
 // layouts
 import RootLayout from "./layouts/RootLayout";
 import HelpLayout from "./layouts/HelpLayout"; 
+import CareersLayout from "./layouts/CareersLayout";
 
 
 const router = createBrowserRouter(
@@ -25,7 +30,23 @@ const router = createBrowserRouter(
         <Route path="faq" element={<Faq />} />
         <Route path="contact" element={<Contact />} />
       </Route>
+
+        {/* //Loader Example  fetch data in Careers.js*/}
+        <Route path="careers" element={<CareersLayout />}>
+        <Route 
+          index 
+          element={<Careers />} 
+          loader={careersLoader} 
+          //Loader inside Careers.js
+          
+        />
+      </Route>
+
+        {/* //404 not found route */}
+      <Route path="*" element={<NotFound />} />
     </Route>
+
+    
   )
 );
 
